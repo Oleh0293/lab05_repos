685d49e111afd969b152c89e1bb5c246e443c426
Oleh0293 <oleh.kulyk.ik.2021@lpnu.ua>
2021-10-20 20:24:17 +0300

    first commit

---
+++ Lab 5_1.cpp
@@ -0,0 +1,21 @@
+// Lab 5_1
+#include <iostream>
+#include <cmath>
+using namespace std;
+double g(const double a, const double b); // прототип
+int main() {
+    double s, t;
+
+    cout << "s = "; cin >> s;
+    cout << "t = "; cin >> t;
+
+    double c = g(pow(s,2), t  + 1) + g(pow(t,2), s + 1);
+
+    cout << "c = " << c << endl;
+
+    return 0;
+}
+double g(const double a, const double b) // визначення
+{
+return (sin(a*b) / pow(a,2) + pow(b,2));
+}
